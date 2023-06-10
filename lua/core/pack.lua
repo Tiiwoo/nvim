45da019d88da5f0f171066ef50b944be48ac1f6a
Tiiwoo <tiiwoo.taowei@outlook.com>
2023-06-10 21:04:24 +0800

    fix clone error

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -57,7 +57,7 @@ end
 
 function Lazy:load_lazy()
 	if not vim.loop.fs_stat(lazy_path) then
-		local lazy_repo = use_ssh and "git@github.com:folke/lazy.nvim.git " or "https://github.com/folke/lazy.nvim.git "
+		local lazy_repo = "https://github.com/folke/lazy.nvim.git "
 		api.nvim_command("!git clone --filter=blob:none --branch=stable " .. lazy_repo .. lazy_path)
 	end
 	self:load_plugins()
