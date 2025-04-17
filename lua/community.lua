081b58c50ab789dd32f100374a3d7e83f5976d99
tiiwoo <tiiwoo.taowei@outlook.com>
2025-04-17 16:07:43 +0800

    feat: back to initial template

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -8,11 +8,5 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.recipes.telescope-nvchad-theme" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.nix" },
-  -- { import = "astrocommunity.pack.zig" },
-  -- { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   -- import/override with your plugins folder
 }
--- lua/plugins/toggleterm.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  opts = function(_, opts)
-    opts.on_open = function(term)
-      vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<Esc>", "<cmd>close<CR>", { noremap = true, silent = true })
-    end
-  end,
-}
